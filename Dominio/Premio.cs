e091813ef5f4c43564d372f3684029f5f15c0687
tamicaa-mat <tamicaa.mat@gmail.com>
2024-10-02 09:32:09 -0300

    agrego clases a dominio

---
+++ Dominio/Premio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Premio
+    {
+        public int Id { get; set; }  // Identificador único del premio
+        public string Nombre { get; set; }  // Nombre del premio
+        public string Descripcion { get; set; }  // Descripción del premio
+        public decimal Valor { get; set; }  // Valor monetario o estimado del premio
+        public List<string> Imagenes { get; set; }  // Lista de URLs o rutas de imágenes del premio
+
+        public Premio()
+        {
+            Imagenes = new List<string>();  // Inicializa la lista de imágenes
+        }
+    }
+}
---
+++ Dominio/Voucher.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    
+    
+        public class Voucher
+        {
+            public string Codigo { get; set; }  
+            public bool Usado { get; set; } 
+        
+          
+            public Cliente Cliente { get; set; }  // Cliente que utilizó el voucher (relación)
+
+           // public Premio PremioSeleccionado { get; set; }  // Premio seleccionado por el cliente (relación)
+
+            public bool EsValido()
+            {
+                // Lógica para verificar si el voucher es válido (por ejemplo, no usado y dentro del período de validez)
+                return !Usado;
+            }
+
+           // public void MarcarComoUsado()
+           // {
+             //   Usado = true;
+               // FechaUso = DateTime.Now;
+           // }
+        }
+
+
+
+
+
+
+
+
+
+    }
+
