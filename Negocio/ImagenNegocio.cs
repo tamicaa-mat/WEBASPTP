d202e2148522d0fa9e12144daadc64c32ef370c1
MayraGonzalezN <mayragonzalezdd@gmail.com>
2024-10-02 22:22:11 -0300

    Detalles

--- Negocio/ImagenNegocio.cs
+++ Negocio/ImagenNegocio.cs
@@ -8,81 +8,78 @@ using System.Threading.Tasks;
 
 namespace Negocio
 {
-    class ImagenNegocio
+    public class ImagenNegocio
     {
-        public class NegocioImagen
+        public List<Imagen> listar(int idArticulo)
         {
-            public List<Imagen> listar(int idArticulo)
+            List<Imagen> lista = new List<Imagen>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
             {
-                List<Imagen> lista = new List<Imagen>();
-                AccesoDatos datos = new AccesoDatos();
+                datos.setearConsulta("SELECT Id, IdArticulo, ImagenUrl FROM IMAGENES WHERE IdArticulo = @IdArticulo");
+                datos.SetearParametro("@IdArticulo", idArticulo);
+                datos.ejecutarLectura();
 
-                try
+                while (datos.Lector.Read())
                 {
-                    datos.setearConsulta("SELECT Id, IdArticulo, ImagenUrl FROM IMAGENES WHERE IdArticulo = @IdArticulo");
-                    datos.SetearParametro("@IdArticulo", idArticulo);
-                    datos.ejecutarLectura();
+                    Imagen imagen = new Imagen();
+                    imagen.Id = (int)datos.Lector["Id"];
+                    imagen.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    imagen.Url = datos.Lector["ImagenUrl"].ToString();
 
-                    while (datos.Lector.Read())
-                    {
-                        Imagen imagen = new Imagen();
-                        imagen.Id = (int)datos.Lector["Id"];
-                        imagen.IdArticulo = (int)datos.Lector["IdArticulo"];
-                        imagen.Url = datos.Lector["ImagenUrl"].ToString();
-
-                        lista.Add(imagen);
-                    }
-                    return lista;
+                    lista.Add(imagen);
                 }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
-                finally
-                {
-                    datos.cerrarConexion();
-                }
+                return lista;
             }
-
-            public void agregar(Imagen nuevaImagen)
+            catch (Exception ex)
             {
-                AccesoDatos datos = new AccesoDatos();
-
-                try
-                {
-                    datos.setearConsulta("INSERT INTO IMAGENES (IdArticulo, ImagenUrl) VALUES (@IdArticulo, @ImagenUrl)");
-                    datos.SetearParametro("@IdArticulo", nuevaImagen.IdArticulo);
-                    datos.SetearParametro("@ImagenUrl", nuevaImagen.Url);
-                    datos.ejecutarAccion();
-                }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
-                finally
-                {
-                    datos.cerrarConexion();
-                }
+                throw ex;
             }
-
-            public void eliminar(int id)
+            finally
             {
-                AccesoDatos datos = new AccesoDatos();
+                datos.cerrarConexion();
+            }
+        }
 
-                try
-                {
-                    datos.setearConsulta("DELETE FROM IMAGENES WHERE Id = @Id");
-                    datos.SetearParametro("@Id", id);
-                    datos.ejecutarAccion();
-                }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
-                finally
-                {
-                    datos.cerrarConexion();
-                }
+        public void agregar(Imagen nuevaImagen)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("INSERT INTO IMAGENES (IdArticulo, ImagenUrl) VALUES (@IdArticulo, @ImagenUrl)");
+                datos.SetearParametro("@IdArticulo", nuevaImagen.IdArticulo);
+                datos.SetearParametro("@ImagenUrl", nuevaImagen.Url);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("DELETE FROM IMAGENES WHERE Id = @Id");
+                datos.SetearParametro("@Id", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
             }
         }
     }
